189c6f94f3abaf189998587ceb6cfdbfc257867b
ArunIonixx <115460821+ArunIonixx@users.noreply.github.com>
2023-05-12 10:34:15 +0530

    Add files via upload

---
+++ scripts/dutchAuction/rebate.ts
@@ -0,0 +1,38 @@
+import { ethers, network } from "hardhat";
+
+async function main() {
+    
+    // network and User object
+    const [owner] = await ethers.getSigners();
+    console.log("Deploying to network:", network.name);
+    console.log("Deploying from account:", owner.address);
+  
+    // Dutch Auction contract Address     
+    const AuctionContractAddress = "0xED30658e32709f2292B1fa5dc3f20951D87F4D9f"; 
+
+    // getting the reference of the deployed Dutch Auction Contract
+    const Auction = await ethers.getContractAt(
+        "DutchAuction",
+        AuctionContractAddress
+    );
+
+    console.log("Auction Contract Deployed on :", Auction.address);
+
+    const auctionId = "3f594d54-e14d-40fe-99e4-c86bb3cf96";     // Auction Id
+    const buyerWalletAddress = ""                               // buyer wallet address
+   
+    // call to buy the token(s)
+    let receipt = await Auction.rebate(auctionId, buyerWalletAddress);
+
+    console.log(`\n transaction hash of created sale, tx hash: ${receipt.hash}`);
+    console.log("Waiting for confirmations...");
+    const tx = await receipt.wait(1);
+    console.log(`Confirmed! Gas used: ${tx.gasUsed.toString()}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
