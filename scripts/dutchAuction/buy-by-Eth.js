5524602e1d89125648dec5c73f3931935a772a25
ArunIonixx <arunkumar.s@ionixxtexh.co>
2023-08-26 00:11:13 +0530

    Latest update

--- scripts/dutchAuction/buy-by-Eth.js
+++ scripts/dutchAuction/buy-by-Eth.js
@@ -6,10 +6,10 @@ const DutchUtilityAbi = require('./DutchUtilityAbi.json');
 /************ Sample Data for buy()******************************
  
 auctionId : "88e0d944-04ab-4ed3-9f8b-c6d2023fd6af"
-buyingAmount : 10000000000000000
+price : 10000000000000000
 tax: 0
-totalBuytoken : 1
-quantity : 0
+quantity : 1
+unit1155 : 0
 BlacklistProof : [0x0000000000000000000000000000000000000000000000000000000000000000]  
 discountPercentage :0
 expirationTime :1689445810
@@ -21,9 +21,9 @@ signer:0xe3d32951C8BA72198207c2F36913aFA5ccA39476
 async function main() {
   
   // Dutch Auction contract Address     
-  const auctionContractAddress = "0x30a32FBA51E2edFdF124c4ff9Bc3824384DB8B1f";
+  const auctionContractAddress = "0xdEB93a6d82206A9e65b45f61E008b77F35f005Ac";
   // Dutch Utility contract Address 
-  const auctionUtilityAddress = "0xa830628c31D3647a73585eECD7008cdb7C97d994";    
+  const auctionUtilityAddress = "0x502cD781ba27f8de47E45e571F64dbDfbfeE5A00";    
   
   // Connect to the network using the ABI and the signer 
   const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
@@ -47,10 +47,10 @@ async function main() {
   // getting the current dutch auction price
   const currentPrice = await AuctionUtility.getCurrentDutchPrice(auctionId);
 
-  const totalBuytoken = 2;                                                    // token count to buy    
-  const buyingAmount = currentPrice.mul(totalBuytoken);                       // calcualted Buying amount based on current price and no of tokens
+  const quantity = 2;                                                    // token count to buy    
+  const price = currentPrice.mul(totalBuytoken);                       // calcualted Buying amount based on current price and no of tokens
   const tax = 0;                                                              // tax of buying Amount.
-  const quantity = 0;                                                         // Quantity should be zero for ERC721
+  const unit1155 = 0;                                                         // Quantity should be zero for ERC721
   const BlacklistProof = ethers.constants.HashZero;                           // default should be zero
   const discountPercentage = 0;                                               // Default should be zero
   const expirationTime = 1689445810;                                          // TimeStamp Value
@@ -64,10 +64,10 @@ async function main() {
   // call to buy the token(s)
   let receipt = await Auction.buy(
     auctionId,
-    buyingAmount,
+    price,
     tax,
-    totalBuytoken,
     quantity,
+    unit1155,
     [BlacklistProof],    
     [discountPercentage, expirationTime, nonce, signature, signer],
     { value: buyingAmount.add(tax) }
